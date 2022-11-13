210b5d3a6475260c0f941056fcb224abda725079
Thijmen Brand <thijmen@ik.nu>
2022-11-13 19:35:59 +0100

    implemented the error manager package

--- src/utils/dom-defaults.ts
+++ src/utils/dom-defaults.ts
@@ -13,11 +13,3 @@ export const appIcon = `<div class="app javascript-os-file-icon-wrapper">
                 <p id='${fileIconSelectors.fileIconTitle}'></p>
               </div>
             `;
-
-export const fatalError = `<div class='fatal-blue-screen-error'>
-              <span class='errorMessageContainer'>
-                <span class='errorEmoji'>╚(•⌂•)╝</span>
-                <span class='error-header'>A fatal error occured</span>
-                <span class='error-body'>Error code: #00001</span>
-              </span>
-            </div>`;
