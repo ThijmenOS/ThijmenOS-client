920471ec608285557814b9a93cdca0a78b7e6fba
Thijmen Brand <thijmen@ik.nu>
2023-01-06 15:03:51 +0100

    permissions

--- src/core/kernel/commands/filesystem/mkdirCommand.ts
+++ src/core/kernel/commands/filesystem/mkdirCommand.ts
@@ -1,9 +1,11 @@
-import { Mkdir } from "@thijmen-os/common";
+import { Mkdir, Permissions } from "@thijmen-os/common";
 import { ICommand } from "@ostypes/CommandTypes";
 import { MakeDirectory } from "@providers/filesystemEndpoints/filesystem";
 
 class mkdirCommand implements ICommand {
   private props: Mkdir;
+
+  readonly requiredPermission = Permissions.fileSystem;
 
   constructor(props: Mkdir) {
     this.props = props;
--- src/core/kernel/commands/filesystem/rmdirCommand.ts
+++ src/core/kernel/commands/filesystem/rmdirCommand.ts
@@ -1,9 +1,11 @@
-import { Path } from "@thijmen-os/common";
+import { Path, Permissions } from "@thijmen-os/common";
 import { ICommand } from "@ostypes/CommandTypes";
 import { RemoveDirectory } from "@providers/filesystemEndpoints/filesystem";
 
 class rmdirCommand implements ICommand {
   private props: Path;
+
+  readonly requiredPermission = Permissions.fileSystem;
 
   constructor(props: Path) {
     this.props = props;
